8e47fb90e370402c785776ff5639b476d50455d5
AlexKoup <129233861+AlexKoup@users.noreply.github.com>
2024-11-04 14:39:44 +0300

    Fix standart name of VLESS inbound
    Update marzban_api.py

--- bot/utils/marzban_api.py
+++ bot/utils/marzban_api.py
@@ -14,7 +14,7 @@ PROTOCOLS = {
         {
             "flow": "xtls-rprx-vision"
         },
-        ["VLESS Reality Steal Oneself"]
+        ["VLESS TCP REALITY"]
     ],
     "trojan": [
         {},
